5a2bf5edba33fc0f10dbc0122f3466a808c4aa99
Chớp BK <dinhtam94@gmail.com>
2019-12-06 22:11:21 +0700

    build a UI in home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN-Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://github.com/chopbk" target="_blank">
+              <img src={logo} width="30" height="30" alt="github.com/chopbk" />
+            </a>
+            <Link to="/" className="navbar-brand">Trang Chủ</Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Công việc</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Tạo công việc</Link>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Tìm kiếm" aria-label="Search" />
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm Kiếm</button>
+              </form>
+            </div>
+          </nav>
         </div>
       </Router>
     );
