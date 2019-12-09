8d03679ab00411ba5e930f2ad446513738fb25b9
Chớp BK <dinhtam94@gmail.com>
2019-12-09 18:17:21 +0700

    make axios request for create todo components

--- client/src/components/create-todo.component.js
+++ client/src/components/create-todo.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateTodo extends Component {
     constructor(props) {
@@ -8,7 +9,8 @@ export default class CreateTodo extends Component {
             todo_description: '',
             todo_responsible: '',
             todo_priority: '',
-            todo_completed: false
+            todo_completed: false,
+            msg: ''
         }
         // make sure to bind those methods to this 
         this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
@@ -41,19 +43,40 @@ export default class CreateTodo extends Component {
         console.log(`Todo Description: ${this.state.todo_description}`);
         console.log(`Todo Responsible: ${this.state.todo_responsible}`);
         console.log(`Todo Priority: ${this.state.todo_priority}`);
-
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        }
+        axios.post('http://localhost:4000/todos/add', newTodo)
+            .then(res => {
+                console.log(res.data)
+                this.setState({ msg: 'todo added successfully' });
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({ msg: 'todo add failed.' });
+            })
         this.setState({
             todo_description: '',
             todo_responsible: '',
             todo_priority: '',
-            todo_completed: false
+            todo_completed: false,
         })
+
     }
     render() {
+        const { msg } = this.state;
         return (
             <div style={{ marginTop: 10 }}>
                 <h3>Tạo công việc mới</h3>
                 <form onSubmit={this.onSubmit}>
+                    {msg !== '' &&
+                        <div class="alert alert-warning alert-dismissible" role="alert">
+                            {msg}
+                        </div>
+                    }
                     <div className="form-group">
                         <label>Mô tả: </label>
                         <input type="text"
@@ -77,7 +100,7 @@ export default class CreateTodo extends Component {
                                 type="radio"
                                 name="priorityOptions"
                                 id="priorityLow"
-                                value="Thấp"
+                                value="Low"
                                 checked={this.state.todo_priority === 'Low'}
                                 onChange={this.onChangeTodoPriority}
                             />
@@ -88,7 +111,7 @@ export default class CreateTodo extends Component {
                                 type="radio"
                                 name="priorityOptions"
                                 id="priorityMedium"
-                                value="Trung Bình"
+                                value="Medium"
                                 checked={this.state.todo_priority === 'Medium'}
                                 onChange={this.onChangeTodoPriority}
                             />
@@ -99,14 +122,13 @@ export default class CreateTodo extends Component {
                                 type="radio"
                                 name="priorityOptions"
                                 id="priorityHigh"
-                                value="Cao"
+                                value="High"
                                 checked={this.state.todo_priority === 'High'}
                                 onChange={this.onChangeTodoPriority}
                             />
                             <label className="form-check-label">High</label>
                         </div>
                     </div>
-
                     <div className="form-group">
                         <input type="submit" value="Tạo công việc" className="btn btn-primary" />
                     </div>
