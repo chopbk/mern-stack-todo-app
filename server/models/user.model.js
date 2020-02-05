58b75bafd42f09c79ab7f578cf477f001dc3f390
Chớp BK <dinhtam94@gmail.com>
2020-02-05 22:11:31 +0700

    server: add encrypt + compare  password in user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
 const Schema = mongoose.Schema;
 //Create Schema
 const UserSchema = new Schema({
@@ -18,7 +20,23 @@ const UserSchema = new Schema({
         type: Date,
         default: Date.now
     }
-
-
 });
+UserSchema.pre('save', async function save(next) {
+    user = this;
+    console.log(user);
+    try {
+        const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+        user.password = await bcrypt.hash(user.password, salt);
+        console.log("pre save ok");
+        return next;
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+});
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    user = this;
+    return await bcrypt.compare(candidatePassword, user.password);;
+
+};
 module.exports = User = mongoose.model("users", UserSchema);
