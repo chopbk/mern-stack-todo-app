3091171f87dc2a230b88ea5ab936061064c221b0
Chớp BK <dinhtam94@gmail.com>
2020-02-14 10:45:56 +0700

    client: add navbar, login, register components

--- client/src/components/layout/navbar.component.js
+++ client/src/components/layout/navbar.component.js
@@ -58,30 +58,3 @@ class Navbar extends Component {
 }
 
 export default Navbar;
-
-/**
-* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="https://github.com/chopbk" target="_blank" rel="noopener noreferrer">
-                <img src={logo} width="30" height="30" alt="github.com/chopbk" />
-            </a>
-
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collpase navbar-collapse" id="navbarSupportedContent">
-                <Link to="/" className="navbar-brand">Trang Chủ</Link>
-                <ul className="navbar-nav mr-auto">
-                    <li className="navbar-item">
-                        <Link to="/todos" className="nav-link">Công việc</Link>
-                    </li>
-                    <li className="navbar-item">
-                        <Link to="/todos/create" className="nav-link">Tạo công việc</Link>
-                    </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Tìm kiếm" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm Kiếm</button>
-                </form>
-            </div>
-        </nav>
-        */
