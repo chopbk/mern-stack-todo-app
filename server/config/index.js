70e6090b559cd4572df0f804019e1a6313cc18a5
chopbk <dinhtam94@gmail.com>
2020-03-15 00:40:00 +0700

    [server] modify config file and add logger winston [chopbk]

---
+++ server/config/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    dev: {
+        mongoURI: 'mongodb+srv://tamnd12:tamhaha123@cluster0-7cxz7.gcp.mongodb.net/todo?retryWrites=true&w=majority',
+        secretOrKey: "secret",
+        log_config: {
+            level: "debug"
+        }
+    },
+    product: {
+        mongoURI: 'mongodb+srv://tamnd12:tamhaha123@cluster0-7cxz7.gcp.mongodb.net/todo?retryWrites=true&w=majority',
+        secretOrKey: "secret",
+        log_config: {
+            level: "debug"
+        }
+    }
+};
